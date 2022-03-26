41e27e650968d32d74df622dffeb562d17c41067
MI Computer <MI Computer@DESKTOP-PCU1D4T>
2022-03-26 20:44:29 +0530

    1st commit

---
+++ LearnJava/src/P2_Task02_ConstantsinJava.java
@@ -0,0 +1,14 @@
+
+public class P2_Task02_ConstantsinJava {
+
+	public static void main(String[] args) {
+		
+        private static final double = 3.14;
+        
+    
+        
+        
+        
+	}
+
+}
